90bb2a1c9ea8af56ad396d4160b49c0121a32db1
arfan36 <arfanur36@gmail.com>
2022-10-04 23:24:55 +0600

    51-7 Responsive pricing card with Tailwind and Hero icons

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -15,7 +15,7 @@ const Pricing = () => {
         },
         {
             id: 2, name: 'Medium', price: 9.99, features: [
-                'Awesome Feature',
+                'Everything on Free',
                 'Extra Feature',
                 'Unnecessary Feature',
                 'Will never use Feature',
@@ -25,7 +25,7 @@ const Pricing = () => {
         },
         {
             id: 3, name: 'Premium', price: 19.99, features: [
-                'Awesome Feature',
+                'Everything on Premium',
                 'Extra Feature',
                 'Unnecessary Feature',
                 'Will never use Feature',
@@ -37,7 +37,7 @@ const Pricing = () => {
     return (
         <div>
             <h2 className='text-5xl font-bold bg-indigo-300 p-12 text-white'>Best Deal of the Town</h2>
-            <div className='grid grid-cols-3 gap-3'>
+            <div className='grid md:grid-cols-3 gap-3 mt-4'>
                 {
                     pricingOptions.map(option => <PriceOption
                         key={option.id}
