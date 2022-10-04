85d29327028b3e1ad015a62a9b68742d37279164
arfan36 <arfanur36@gmail.com>
2022-10-04 14:09:50 +0600

    51-4 Responsive Navbar using react and tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1 className='text-4xl font-bold underline'>This is a big big header</h1>
       <p>This is just a paragraph</p>
     </div>
